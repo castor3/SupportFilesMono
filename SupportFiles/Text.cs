8a484cb2e080814a5470249d26e74877a8432178
santosru <santosru@DK966360>
2017-12-12 18:28:02 +0000

    Add "RemoveDiacritics()" and "WriteToLogFile()"

---
+++ SupportFiles/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportFiles
+{
+	public class Text
+	{
+		public static string RemoveDiacritics(string text)
+		{// Replaces accented letters with equivalent ones (normalizes the string)
+			if (string.IsNullOrWhiteSpace(text)) return string.Empty;
+
+			var normalizedString = text.Normalize(NormalizationForm.FormD);
+			var stringBuilder = new StringBuilder();
+
+			foreach (var character in normalizedString) {
+				var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(character);
+				if (unicodeCategory != UnicodeCategory.NonSpacingMark) {
+					stringBuilder.Append(character);
+				}
+			}
+			return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+		}
+	}
+}
